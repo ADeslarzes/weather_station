dc2459ba02df1087c943eb12929af80248bfdc8a
ADeslarzes <168564507+ADeslarzes@users.noreply.github.com>
2024-08-09 10:18:52 +0200

    Add requirements

--- bme280_sensor.py
+++ bme280_sensor.py
@@ -9,10 +9,13 @@ bus = smbus2.SMBus(port)
 bme280.load_calibration_params(bus,address)
 
 def read_all():
-    bme280_data = bme280.sample(bus,address)
-    humidity  = bme280_data.humidity
-    pressure  = bme280_data.pressure
-    ambient_temperature = bme280_data.temperature
-    print("Humidity: ", humidity, " pressure: ", pressure," temperature: ", ambient_temperature)
-    sleep(1)
+  bme280_data = bme280.sample(bus,address)
+  humidity  = bme280_data.humidity
+  pressure  = bme280_data.pressure
+  ambient_temperature = bme280_data.temperature
+  print("Humidity: ", humidity, " pressure: ", pressure," temperature: ", ambient_temperature)
+  sleep(1)
+
+# while True:
+#   read_all()
     
